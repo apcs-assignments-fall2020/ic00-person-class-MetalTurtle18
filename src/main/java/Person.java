2d2022aa0fe91bb43963618aa98d415d9ceb6788
MetalTurtle <dekolis1218@gmail.com>
2020-10-27 08:21:45 -0700

    Complete Person class and client

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,19 @@
 public class Person {
-    
+    private String firstName;
+    private String lastName;
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void doSomething() {
+        System.out.println("I am watching Netflix");
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person john = new Person("John", "Smith");
+        john.doSomething();
+        System.out.println(john);
     }
 }
